40d935f67dbfcd279d7181706a2c649372c198da
Ade1fe <addypearl09@gmail.com>
2023-05-12 15:02:40 +0100

    all done

--- script.js
+++ script.js
@@ -8,3 +8,23 @@
             document.body.classList.remove('active');
         }
     }
+
+
+    // image slider
+     var img1 = document.getElementById("image1");
+  
+    var imageSources = [
+        "/imgs/img1.jpg",
+        "/imgs/img2.jpg",
+        "/imgs/img3.jpg" 
+    ];
+
+    var currentIndex = 0;
+
+    function changeImages() {
+        img1.src = imageSources[currentIndex];
+        currentIndex = (currentIndex + 1) % imageSources.length;
+        img1.src = imageSources[currentIndex];
+    }
+
+    setInterval(changeImages, 5000);
